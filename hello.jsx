051b63921bf5af7c518132859cc04a4024f53dff
Alastair Mory <alastair.mory@gmail.com>
2018-02-14 19:38:54 +0800

    Moved header to app.js.

--- hello.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-export default class Hello extends React.Component{
-  render(){
-    return <h1>RBA Interest Rate Decision Tracker</h1>;
-  }
-}
